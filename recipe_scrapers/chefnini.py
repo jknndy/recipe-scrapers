eb751c5233b36813d5c02dd8e12a04e8179020af
Joey <7505194+jknndy@users.noreply.github.com>
2024-12-22 16:20:01 -0500

    groupings

--- recipe_scrapers/chefnini.py
+++ recipe_scrapers/chefnini.py
@@ -1,7 +1,16 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class Chefnini(AbstractScraper):
     @classmethod
     def host(cls):
         return "chefnini.com"
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            "h3:not([itemprop='recipeYield'])",
+            "ul li[itemprop='ingredients']",
+        )
