185dc1917edd1913e31508205866a64f090e1c58
Joey <7505194+jknndy@users.noreply.github.com>
2025-08-22 16:39:14 -0400

    instructions fix

--- recipe_scrapers/chefkoch.py
+++ recipe_scrapers/chefkoch.py
@@ -5,3 +5,10 @@ class Chefkoch(AbstractScraper):
     @classmethod
     def host(cls):
         return "chefkoch.de"
+
+    def instructions(self):
+        instruction_elements = self.soup.select(
+            'span.instruction__text[data-testid="recipe-instruction"]'
+        )
+        steps = [element.get_text(strip=True) for element in instruction_elements]
+        return "\n".join(steps)
