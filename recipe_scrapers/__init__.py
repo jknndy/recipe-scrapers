be97ab8f34d2caa431803ecbfb33e3cb58f8c7a4
Valkyrie1248 <228403139+valkyrie1248@users.noreply.github.com>
2026-03-18 14:37:20 -0600

    Add support for Post Punk Kitchen (#1854)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -443,6 +443,7 @@ from .plowingthroughlife import PlowingThroughLife
 from .polishfoodies import PolishFoodies
 from .poppycooks import PoppyCooks
 from .popsugar import PopSugar
+from .postpunkkitchen import PostPunkKitchen
 from .potatorolls import PotatoRolls
 from .practicalselfreliance import PracticalSelfReliance
 from .preppykitchen import PreppyKitchen
@@ -1066,6 +1067,7 @@ SCRAPERS = {
     PolishFoodies.host(): PolishFoodies,
     PoppyCooks.host(): PoppyCooks,
     PopSugar.host(): PopSugar,
+    PostPunkKitchen.host(): PostPunkKitchen,
     PotatoRolls.host(): PotatoRolls,
     PracticalSelfReliance.host(): PracticalSelfReliance,
     PreppyKitchen.host(): PreppyKitchen,
