73ab1686694155cff7aae4ad233b6cd1274c89c4
daniel <51932404+d-buckner@users.noreply.github.com>
2026-04-29 15:16:25 -0700

    Add scraper: mollybaz.com (#1914)
    * Add scraper: mollybaz.com
    
    * linting
    
    ---------

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -390,6 +390,7 @@ from .misya import Misya
 from .mob import Mob
 from .mobkitchen import MobKitchen
 from .modernhoney import ModernHoney
+from .mollybaz import MollyBaz
 from .momontimeout import MomOnTimeout
 from .momswithcrockpots import MomsWithCrockPots
 from .moscatomom import MoscatoMom
@@ -1029,6 +1030,7 @@ SCRAPERS = {
     Mob.host(): Mob,
     MobKitchen.host(): MobKitchen,
     ModernHoney.host(): ModernHoney,
+    MollyBaz.host(): MollyBaz,
     MomOnTimeout.host(): MomOnTimeout,
     MomsWithCrockPots.host(): MomsWithCrockPots,
     MoscatoMom.host(): MoscatoMom,
